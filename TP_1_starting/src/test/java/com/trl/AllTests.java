0ad7f05068da292c50f6cbe0fb9ecb88ff5a5b64
Mike Pone <mike.a.pone@gmail.com>
2017-05-11 17:42:12 -0500

    ordered tests alphabetically

--- TP_1_starting/src/test/java/com/trl/AllTests.java
+++ TP_1_starting/src/test/java/com/trl/AllTests.java
@@ -5,9 +5,10 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ CheckInControllerTest.class, CheckOutControllerTest.class, CopyTest.class, DataStoreTest.class,
-		HoldTest.class, PatronTest.class, SellCopyControllerTest.class, TextbookTest.class, 
-		ManageHoldControllerTest.class, ControllerTest.class, PayFineControllerTest.class })
+@SuiteClasses({ CheckInControllerTest.class, CheckOutControllerTest.class, ControllerTest.class, CopyTest.class, 
+	DataStoreTest.class, HoldTest.class, ManageHoldControllerTest.class, PatronTest.class, PayFineControllerTest.class, 
+	SellCopyControllerTest.class, TextbookTest.class, 
+		  })
 public class AllTests {
 
 }
